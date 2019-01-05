ac640b6a07bc44bf4e54e9ad96f28202be49d1ab
Rio6 <rio6.liu@gmail.com>
2019-01-04 23:31:09 -0800

    Render the ship better

--- render.js
+++ render.js
@@ -12,36 +12,22 @@ var ctx = canvas.getContext('2d');
 
 loadImage("atlas.png").then((image) => atlas = image);
 
-var drawImage = (file, x, y, w = SIZE, h = SIZE, r = 0, flip = false) => {
+var drawImage = (file, x, y, w = SIZE, h = SIZE, dir = 0, flip = false, color, colorMode) => {
     if(!atlas) {
         console.log("Not ready");
         return;
     }
 
-    if(!mappings[file]) {
-        console.log("not in mappings", file);
-        return;
-    }
+    let img = getImage(file, flip, color, colorMode);
 
-    let uv = mappings[file].uv;
-    let x0 = uv[0] * atlasSize;
-    let y0 = (1 - uv[1]) * atlasSize;
-    let x1 = uv[2] * atlasSize;
-    let y1 = (1 - uv[3]) * atlasSize;
-    let ws = x1 - x0;
-    let hs = y1 - y0;
+    ctx.save();
+    ctx.translate(x + w / 2, y + h / 2);
+    ctx.rotate(-dir * Math.PI / 2);
+    ctx.translate(-x - w / 2, -y - h / 2);
 
-    if(flip)
-        ctx.setTransform(-1, 0, 0, 1, x + w, y);
-    else
-        ctx.setTransform(1, 0, 0, 1, x, y);
+    ctx.drawImage(img, x, y, w, h);
 
-    ctx.translate(w / 2, h / 2);
-    ctx.rotate(flip ? -r : r);
-    ctx.translate(-w / 2, -h / 2);
-
-    ctx.drawImage(atlas, x0, y0, ws, hs, 0, 0, w, h);
-    ctx.resetTransform();
+    ctx.restore();
 }
 
 var drawPart = (name, x, y, dir, color) => {
@@ -63,47 +49,19 @@ var drawPart = (name, x, y, dir, color) => {
     let yt = NxN / 2 * SIZE - y - ht / 2;
     let flip = x < 0 && !name.includes("Letter") && !name.includes("Mod");
 
-    drawImage(file, xt, yt, wt, ht, -dir * Math.PI / 2, flip);
+    let mode = null;
+    if(isDecal(name))
+        mode = "color";
+    else if(hasColor(name))
+        mode = "replace";
 
-    if(dir % 2 === 1) {
-        xt += wt / 2 - ht / 2; yt += ht / 2 - wt / 2;
-        t = wt; ht = wt; wt = t;
-    }
+    drawImage(file, xt, yt, wt, ht, dir, flip, color, mode);
 
-    let changeColor = null;
-    /*
-    if(isDecal(name)) {
-        changeColor = (data, i) => {
-            if(data[i] + data[i+1] + data[i+2] === 255 * 3) {
-                data[i] = data[i] / 255 * color[0];
-                data[i+1] = data[i+1] / 255 * color[1];
-                data[i+2] = data[i+2] / 255 * color[2];
-            }
-        };
-    } else if(hasColor(name)) {
-    */
-    if(isDecal(name) || hasColor(name)) {
-        changeColor = (data, i) => {
-            if(data[i+1] === data[i+2] && data[i] > data[i+1]) {
-                let p = data[i] / (data[i] + data[i+1] + data[i+2]);
-                data[i] = p * color[0] + (1-p) * data[i];
-                data[i+1] = p * color[1] + (1-p) * data[i+1];
-                data[i+2] = p * color[2] + (1-p) * data[i+2];
-            }
-        }
-    }
-
-    let imageData = ctx.getImageData(xt, yt, wt, ht);
-    let data = imageData.data;
-    if(changeColor) {
-        for(let i = 0; i < data.length; i += 4) {
-            changeColor(data, i);
-        }
-    }
-    ctx.putImageData(imageData, xt, yt);
+    if(name === "JumpEngine")
+        drawImage("parts/engineJumpPip.png", xt, yt, wt, ht, -dir * Math.PI / 2, flip);
 }
 
-var drawShip = module.exports.drawShip = (spec, stats, color = [255, 255, 255]) => {
+var drawShip = module.exports.drawShip = (spec, stats, color = [255, 255, 255, 255]) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     ctx.globalCompositeOperation = "source-over";
@@ -123,19 +81,7 @@ var drawShip = module.exports.drawShip = (spec, stats, color = [255, 255, 255]) 
         let r = stats.radius + 40;
         let x = NxN / 2 * SIZE + stats.center[0] - r;
         let y = NxN / 2 * SIZE - stats.center[1] - r;
-        drawImage("img/point02.png", x, y, r * 2, r * 2);
-
-        let imageData = ctx.getImageData(x, y, r * 2, r * 2);
-        let data = imageData.data;
-        for(let i = 0; i < data.length; i += 4) {
-            if(255 * 3 - (data[i] + data[i+1] + data[i+2]) < 200) {
-                data[i] = (data[i] + color[0]) / 2;
-                data[i+1] = (data[i+1] + color[1]) / 2;
-                data[i+2] = (data[i+2] + color[2]) / 2;
-                data[i+3] = 255;
-            }
-        }
-        ctx.putImageData(imageData, x, y);
+        drawImage("img/point02.png", x, y, r * 2, r * 2, 0, false, color, "color");
     }
 
     for(let part of spec.parts) {
@@ -145,6 +91,53 @@ var drawShip = module.exports.drawShip = (spec, stats, color = [255, 255, 255]) 
     //require("child_process").spawn("firefox", [canvas.toDataURL()]);
     return canvas.toBuffer("image/png");
 }
+
+var getImage = (file, flip = false, color, colorMode) => {
+
+    if(!mappings[file]) {
+        console.log("not in mappings", file);
+        return;
+    }
+
+    let uv = mappings[file].uv;
+    let x = uv[0] * atlasSize;
+    let y = (1 - uv[1]) * atlasSize;
+    let x1 = uv[2] * atlasSize;
+    let y1 = (1 - uv[3]) * atlasSize;
+    let w = x1 - x;
+    let h = y1 - y;
+
+    let cCanvas = createCanvas(w, h);
+    let cCtx = cCanvas.getContext('2d');
+
+    if(flip)
+        cCtx.setTransform(-1, 0, 0, 1, w, 0);
+
+    cCtx.drawImage(atlas, x, y, w, h, 0, 0, w, h);
+
+    if(color && colorMode) {
+        let imageData = cCtx.getImageData(0, 0, w, h);
+        let data = imageData.data;
+        for(let i = 0; i < data.length; i += 4) {
+            // I have no idea what these called so I made them up
+            if(colorMode === "color") {
+                data[i] = data[i] * color[0] / 255;
+                data[i+1] = data[i+1] * color[1] / 255;
+                data[i+2] = data[i+2] * color[2] / 255;
+                //data[i+3] = 255;
+            } else if(colorMode === "replace") {
+                if(data[i+1] === data[i+2] && data[i] > data[i+1]) {
+                    let p = data[i] / (data[i] + data[i+1] + data[i+2]);
+                    data[i] = p * color[0] + (1-p) * data[i];
+                    data[i+1] = p * color[1] + (1-p) * data[i+1];
+                    data[i+2] = p * color[2] + (1-p) * data[i+2];
+                }
+            }
+        }
+        cCtx.putImageData(imageData, 0, 0);
+    }
+    return cCanvas;
+};
 
 var isDecal = (name) => {
     return name.includes("Decal") || name.includes("Letter") || name.includes("Stripe");
@@ -157,7 +150,7 @@ var hasColor = (name) => {
 /*
 setTimeout(() => {
     let spec = JSON.parse(process.argv[2]);
-    drawShip(spec);
+    drawShip(spec, {shield:0}, [80, 80, 80, 255]);
 }, 500);
 */
 
